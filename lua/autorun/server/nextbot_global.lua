5d122c4feabb8b42eb48c4ef311373b681e623a8
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-13 20:42:27 -0400

    fix NULL bug

--- lua/autorun/server/nextbot_global.lua
+++ lua/autorun/server/nextbot_global.lua
@@ -2,6 +2,8 @@ ScareHealthThreshold = CreateConVar("pvox_nextbot_scared_threshold", "100000", {
 RegularRadius        = CreateConVar("pvox_nextbot_scared_radius", "650", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "A majority of scariness functionality depends on this value.", 0, 1000000)
 
 function Scary(ent)
+    if ! ent then return end
+    if ! IsValid(ent) then return end
     -- the requirements for an entity to be considered "Scary"
     return ent:Health() > ScareHealthThreshold:GetInt()
 end
